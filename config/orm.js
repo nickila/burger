c4f7c6ccdeef641fd589311ce1562bd69e8daa3c
Brian Nickila <brian.nickila@gmail.com>
2019-01-03 14:19:04 -0600

    Made some changes. Burgers are showing up on screen but not able to add burgers to the database yet.

--- config/orm.js
+++ config/orm.js
@@ -19,7 +19,7 @@ var orm = {
         queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
+        // queryString += printQuestionMarks(vals.length);
         queryString += ") ";
 
         console.log(queryString);
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,23 @@
+$(function () {
+    $(".create-form").on("submit", function (event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        // sleepy: $("[name=sleepy]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function () {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
