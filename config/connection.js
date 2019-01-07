31756061c9ee9a4cfa07b8e70073e026f249e0b2
Brian Nickila <brian.nickila@gmail.com>
2019-01-07 16:53:24 -0600

    Trying to get heroku to work.

--- config/connection.js
+++ config/connection.js
@@ -14,13 +14,7 @@ if (process.env.JAWSDB_URL) {
 
 }
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
 // Inside the connection.js file, setup the code to connect Node to MySQL.
