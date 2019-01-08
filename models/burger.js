2cd3f2804161f154e49993837bb36ed5d66f0ee3
Brian Nickila <brian.nickila@gmail.com>
2019-01-07 18:20:14 -0600

    Trying to fix heroku.

--- models/burger.js
+++ models/burger.js
@@ -18,11 +18,11 @@ var burger = {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+//   delete: function(condition, cb) {
+//     orm.delete("burgers", condition, function(res) {
+//       cb(res);
+//     });
+//   }
 };
 module.exports = burger;
 // app.get("/", function (req, res) {
